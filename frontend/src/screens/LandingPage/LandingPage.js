b883f8a5a1633108e3b5a82c03f54fea6f16bc42
Aradhya Garg <aradhyagarg@Aradhyas-MacBook-Air.local>
2023-05-24 22:51:46 +0530

    Landing Page styling

--- frontend/src/screens/LandingPage/LandingPage.js
+++ frontend/src/screens/LandingPage/LandingPage.js
@@ -1,12 +1,33 @@
 import React from 'react'
-import {Container, Row} from 'react-bootstrap'
+import {Container, Row, Button} from 'react-bootstrap'
 import "./LandingPage.css"
 const LandingPage = () => {
   return (
     <div className='main'>
         <Container>
             <Row>
-                Landing
+                <div>
+                  <h1 className='title'>Welcome the Notes</h1>
+                <p className='subtitle'>
+                  One Safe Place For Your Notes
+                </p>
+                </div>
+              <div className="buttonContainer">
+              <a href="/login">
+                <Button size="lg" className="landingbutton">
+                  Login
+                </Button>
+              </a>
+              <a href="/register">
+                <Button
+                  variant="outline-primary"
+                  size="lg"
+                  className="landingbutton"
+                >
+                  Signup
+                </Button>
+              </a>
+            </div>
             </Row>
         </Container>
     </div>
