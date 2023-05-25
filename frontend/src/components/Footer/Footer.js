fbf30b330ae8b56a8b0ec7363786f1288f118b4b
Aradhya Garg <aradhyagarg@Aradhyas-MacBook-Air.local>
2023-05-25 12:07:18 +0530

    added card structure

--- frontend/src/components/Footer/Footer.js
+++ frontend/src/components/Footer/Footer.js
@@ -11,7 +11,7 @@ import {
 export default function Footer() {
   return (
     <div>
-      <MDBFooter className='bg-dark text-center text-white' style={{ position: 'fixed', bottom: 0, width: '100%' }}>
+      <MDBFooter className='bg-primary text-center text-white' style={{ position: 'fixed', bottom: 0, width: '100%' }}>
         <MDBContainer className='p-4 pb-0'>
           <section className='mb-4'>
             <MDBBtn outline color="light" floating className='m-1' href='#!' role='button'>
--- frontend/src/screens/MyNotes/MyNotes.js
+++ frontend/src/screens/MyNotes/MyNotes.js
@@ -1,8 +1,48 @@
 import React from 'react'
+import { Button, Card } from 'react-bootstrap'
+import { Link } from 'react-router-dom'
+import notes from '../../components/data/notes'
+import MainScreen from '../../components/MainScreen'
 
 const MyNotes = () => {
+    const deleteHandler = (id) => {
+        if (window.confirm("Are you sure?")) {
+        }
+      };
   return (
-    <div>MyNotes</div>
+    <MainScreen title="Welcome Back Aradhya Garg">
+        <Link to = "createnote">
+            <Button style={{marginLeft: 10, marginBottom: 6}} size="lg">
+                Create New Node
+            </Button>
+            {notes.map((note) => (
+                <Card style={{ margin: 10 }}>
+                <Card.Header style={{display: "flex"}}>
+                    <span style={{
+                        color: "black",
+                        textDecoration: "none",
+                        flex: 1,
+                        cursor: "pointer",
+                        alignSelf: "center",
+                        fontSize: 18,
+                    }}>
+                         {note.title}
+                    </span>
+                    <div>
+                        <Button href={`/note/${note._id}`}>
+                            Edit
+                        </Button>
+                        <Button variant='danger' className='mx-2' onClick={() => 
+                            deleteHandler(note._id)}>
+                            Delete
+                        </Button>
+                    </div>
+                </Card.Header>
+            </Card>
+            ))}
+            
+        </Link>
+    </MainScreen>
   )
 }
 
